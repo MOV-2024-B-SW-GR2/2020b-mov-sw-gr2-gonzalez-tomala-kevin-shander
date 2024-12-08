0ee77f840b56eda62c0e61323dff22076588a155
ShanderGonzalez <kevin.gonzalez@gaheos.com>
2024-12-08 01:12:03 -0500

    avance clase - ciclo vida

--- 01-Android/app/src/main/java/com/example/gr2sw2024b_ksgt/ACicloVida.kt
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_ksgt/ACicloVida.kt
@@ -5,16 +5,65 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_aciclo_vida)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mostrarMensaje("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarMensaje("onStart\n")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarMensaje("onResume\n")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarMensaje("onPause\n")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarMensaje("onStop\n")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarMensaje("onDestroy\n")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run { putString("variableTextoGuardado", textoGlobal) }
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val textoRecuperado = savedInstanceState.getString("variableTextoGuardado")
+        if(textoRecuperado != null) {
+            mostrarMensaje(textoRecuperado)
+        }
+    }
+
+    var textoGlobal = ""
+    fun mostrarMensaje(texto: String) {
+        textoGlobal += texto
+        var snack = Snackbar.make(findViewById(R.id.cl_ciclo_vida), textoGlobal, Snackbar.LENGTH_INDEFINITE)
+        snack.show()
     }
 }
